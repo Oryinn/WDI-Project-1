452f80cbd4b8c09024bfd6a4dc9c91260a15d396
Jordan Williams <Oryinn@Jordans-MacBook-Pro-6.local>
2019-02-15 14:05:46 -0500

    First round can be clicked and checks

--- js/main.js
+++ js/main.js
@@ -21,22 +21,24 @@ let roundAnswer = 0
 
 function gameStart() {
     correctAnswers.push(Math.floor(Math.random() * 4))
+    currentColor = (correctAnswers[correctAnswers.length-1])
     $("#playarea > ." + currentColor).addClass("flashit")
-
+    
     //$.when(playerTurn()).done(scoringTurn())
     // playerTurn()
     // currentAnswers.push(roundAnswer)
     // scoringTurn()
 }
 
+$("#aside > #start").on('click', function () {
+    gameStart()
+    $("#aside > #start").hide()
+})
 function roundStart() {
     correctAnswers.push(Math.floor(Math.random() * 4))
 
     for (let i = 0; i < correctAnswers.length; i++) {
 
-    }
-    if (correctAnswers === currentAnswers) {
-        playerTurn()
     }
 }
 
@@ -46,6 +48,34 @@ function incorrect() {
 
 }
 
+    $("#playarea > #red").on('click', function () {
+        console.log("Red has been clicked")
+        currentAnswers.push(0)
+        $("#playarea > #red").addClass("flashit")
+        scoringTurn()
+        
+    })
+    $("#playarea > #green").on('click', function () {
+        console.log("Green has been clicked")
+        currentAnswers.push(0)
+        $("#playarea > #green").addClass("flashit")
+        scoringTurn()
+    })
+    $("#playarea > #blue").on('click', function () {
+        console.log("Blue has been clicked")
+        currentAnswers.push(0)
+        $("#playarea > #blue").addClass("flashit")
+        scoringTurn()
+    })
+    $("#playarea > #yellow").on('click', function () {
+        console.log("Yellow has been clicked")
+        currentAnswers.push(0)
+        $("#playarea > #yellow").addClass("flashit")
+        scoringTurn()
+    })
+    
+
+
 function scoringTurn() {
     if (roundAnswer === (correctAnswers.length - 1)) {
         console.log("Correct!")
@@ -53,6 +83,7 @@ function scoringTurn() {
         $("#aside > .score").text(`Score: ${score}`)
         //roundStart()
         // $.when(scoringTurn()).done(roundStart())
+
     }
     else {
         console.log("Incorrect")
@@ -60,30 +91,3 @@ function scoringTurn() {
     }
 
 }
-
-$("#aside > #start").on('click', function () {
-    gameStart()
-    $("#aside > #start").hide()
-})
-
-$("#playarea > #red").on('click', function () {
-    console.log("Red has been clicked")
-    roundAnswer = 0
-    scoringTurn()
-    
-})
-$("#playarea > #green").on('click', function () {
-    console.log("Green has been clicked")
-    roundAnswer = 1
-    scoringTurn()
-})
-$("#playarea > #blue").on('click', function () {
-    console.log("Blue has been clicked")
-    roundAnswer = 2
-    scoringTurn()
-})
-$("#playarea > #yellow").on('click', function () {
-    console.log("Yellow has been clicked")
-    roundAnswer = 3
-    scoringTurn()
-})
